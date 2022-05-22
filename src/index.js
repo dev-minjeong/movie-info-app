d1201c65864f93e97ed0b76175cf45a7334ba5e8
minjeong-dev <vitaminjeong1026@gmail.com>
2022-05-22 23:09:58 +0900

    JSX 실습

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+
+function Title() {
+  return (
+    <h3 id='title' onMouseEnter={() => console.log('enter!')}>
+      Hello I'm a title
+    </h3>
+  );
+}
+const Button = () => (
+  <button
+    style={{ backgroundColor: 'tomato' }}
+    onClick={() => console.log('clicked!')}
+  >
+    Click
+  </button>
+);
+const Container = () => (
+  <div>
+    <Title></Title>
+    <Button></Button>
+  </div>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Container />
   </React.StrictMode>,
   document.getElementById('root')
 );
