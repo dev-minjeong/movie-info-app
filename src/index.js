5b538aa65db7c992a37e2ace36da7fbac3da077b
minjeong-dev <vitaminjeong1026@gmail.com>
2022-05-23 16:29:07 +0900

    props 2가지 방법으로 전달 해보기

--- src/index.js
+++ src/index.js
@@ -3,106 +3,35 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import { useState } from 'react';
 
-function MinutesToHours() {
-  const [amount, setAmount] = useState();
-  const [flipped, setFlipped] = useState(false);
-  const onChange = (event) => {
-    setAmount(event.target.value);
-  };
-  const onReset = () => {
-    setAmount('');
-  };
-  const onFlip = () => {
-    onReset();
-    setFlipped(!flipped);
-  };
+/** props를 전달하는 두가지 방법
+ *  1. 인자를 props로 전달 후 {props.banana}로 접근하기
+ *  2. 인자를 {banana, ...} 로 전달 후 {banana}로 접근하기
+ */
+function Btn(props) {
+  // function Btn({banana, apple}) {
   return (
-    <>
-      <label htmlFor='minutes'>Minutes</label>
-      <input
-        value={flipped ? amount * 60 : amount}
-        id='minutes'
-        placeholder='Minutes'
-        type='number'
-        onChange={onChange}
-        disabled={flipped}
-      ></input>
-      <label htmlFor='hours'>Hours</label>
-      <input
-        value={!flipped ? Math.round(amount / 60) : amount}
-        id='hours'
-        placeholder='Hours'
-        type='number'
-        onChange={onChange}
-        disabled={!flipped}
-      ></input>
-      <button onClick={onReset}>Reset</button>
-      <button onClick={onFlip}>Flip</button>
-    </>
-  );
-}
-function KmToMiles() {
-  const [amount, setAmount] = useState();
-  const [flipped, setFlipped] = useState(false);
-  const onChange = (event) => {
-    setAmount(event.target.value);
-  };
-  const onReset = () => {
-    setAmount('');
-  };
-  const onFlip = () => {
-    onReset();
-    setFlipped(!flipped);
-  };
-  return (
-    <>
-      <label htmlFor='km'>Km</label>
-      <input
-        value={flipped ? Math.round(amount * 1.61) : amount}
-        id='km'
-        placeholder='km'
-        type='number'
-        onChange={onChange}
-        disabled={flipped}
-      ></input>
-      <label htmlFor='miles'>Miles</label>
-      <input
-        value={!flipped ? Math.round(amount * 0.62) : amount}
-        id='miles'
-        placeholder='miles'
-        type='number'
-        onChange={onChange}
-        disabled={!flipped}
-      ></input>
-      <button onClick={onReset}>Reset</button>
-      <button onClick={onFlip}>Flip</button>
-    </>
+    <button
+      style={{
+        backgroundColor: 'salmon',
+        color: props.apple ? 'black' : 'white',
+        // color: apple ? 'black' : 'white',
+        padding: '10px 20px',
+        border: 0,
+        borderRadius: 10,
+        cursor: 'pointer',
+      }}
+    >
+      {props.banana}
+      {/* {banana} */}
+    </button>
   );
 }
 
 function App() {
-  const [select, setSelect] = useState('select');
-  function onSelect(event) {
-    setSelect(event.target.value);
-  }
   return (
     <div>
-      <h1>Super Converter</h1>
-      <select onChange={onSelect}>
-        <option value='select'>선택하세요</option>
-        <option value='minutes'>MinutesToHours</option>
-        <option value='km'>KmToMiles</option>
-      </select>
-      <hr></hr>
-      <div>
-        {select === 'minutes' ? (
-          <MinutesToHours></MinutesToHours>
-        ) : select === 'km' ? (
-          <KmToMiles></KmToMiles>
-        ) : (
-          <h4>변환 단위를 선택하세요!</h4>
-        )}
-      </div>
+      <Btn banana='Submit' apple={false}></Btn>
+      <Btn banana='Reset' apple={true}></Btn>
     </div>
   );
 }
