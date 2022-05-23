eab8426adb08d8845c5430a29f2beb47070cd183
minjeong-dev <vitaminjeong1026@gmail.com>
2022-05-23 16:55:29 +0900

    memo 사용하기

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 import { useState } from 'react';
@@ -7,31 +7,34 @@ import { useState } from 'react';
  *  1. 인자를 props로 전달 후 {props.banana}로 접근하기
  *  2. 인자를 {banana, ...} 로 전달 후 {banana}로 접근하기
  */
-function Btn(props) {
-  // function Btn({banana, apple}) {
+function Btn({ text, changeValue }) {
+  console.log(text);
   return (
     <button
+      onClick={changeValue}
       style={{
         backgroundColor: 'salmon',
-        color: props.apple ? 'black' : 'white',
-        // color: apple ? 'black' : 'white',
+        color: 'white',
         padding: '10px 20px',
         border: 0,
         borderRadius: 10,
         cursor: 'pointer',
       }}
     >
-      {props.banana}
-      {/* {banana} */}
+      {text}
     </button>
   );
 }
+// memo를 활용해서 변경되지 않은 prop은 re-render 되지 않게 설정 가능
+const MemoBtn = memo(Btn);
 
 function App() {
+  const [value, setValue] = useState('Submit');
+  const changeValue = () => setValue('Change');
   return (
     <div>
-      <Btn banana='Submit' apple={false}></Btn>
-      <Btn banana='Reset' apple={true}></Btn>
+      <MemoBtn text={value} changeValue={changeValue}></MemoBtn>
+      <MemoBtn text='Reset'></MemoBtn>
     </div>
   );
 }
