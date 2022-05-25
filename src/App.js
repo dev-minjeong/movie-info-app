5b3ff95a52818271e24e6be3a0e89f6f8af77a2b
minjeong-dev <vitaminjeong1026@gmail.com>
2022-05-25 15:22:07 +0900

    useEffect 사용하기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,63 @@
+import { useState, useEffect } from 'react';
+/** useEffect(() => {}, [])
+ *  처음 렌더링 시 한번만 실행되길 원할때 사용함
+ *  렌더링 후 업데이트 되어도 useEffect 설정한 부분은 실행 안됨
+ *  [] 안에 지정한 값이 변화 시에만 실행함
+ *  [] 안에 지정 값이 없을 시 처음 렌더링 할때 한번만 실행
+ *  useEffect가 destroy시 return에 원하는 콜백함수를 주면 됨 → cleanUp
+ *  정리가 필요한 effect에 cleanUp사용함
+ */
+function App() {
+  const [count, setCount] = useState(0);
+  const [keyword, setkeyword] = useState('');
+  const [text, setText] = useState(false);
+  console.log('render!');
+  const onClick = () => {
+    setCount(() => count + 1);
+  };
+  const onChange = (event) => {
+    setkeyword(event.target.value);
+  };
+  const changeText = () => {
+    setText(!text);
+  };
+  useEffect(() => {
+    console.log('I run only once.');
+  }, []);
+  useEffect(() => {
+    if (keyword.length > 5) {
+      console.log('search for', keyword);
+    }
+    console.log('I run when "keyword" changes.');
+  }, [keyword]);
+  useEffect(() => {
+    console.log('I run when "count" changes.');
+  }, [count]);
+  useEffect(() => {
+    console.log('I run when "keyword & count" changes.');
+  }, [keyword, count]);
+
+  return (
+    <>
+      <input value={keyword} type='text' onChange={onChange}></input>
+      <h1>{count}</h1>
+      <button onClick={onClick}>Click Me!</button>
+      {text ? null : <Greet></Greet>}
+      <button onClick={changeText}>{text ? 'Showing' : 'Hide'}</button>
+    </>
+  );
+}
+function Greet() {
+  useEffect(() => {
+    console.log('hi^^');
+    // cleanUp
+    return () => {
+      console.log('byeTT');
+    };
+  }, []);
+  return <h3>Hi!</h3>;
+}
+export default App;
 /* 
 import {
   BrowserRouter as Router,
@@ -7,6 +67,7 @@ import {
 } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import { useEffect } from 'react';
 
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,43 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import { useState } from 'react';
-import { PropTypes } from 'prop-types';
-
-function Btn({ text, fontSize = 16 }) {
-  // props의 기본값 설정 가능
-  return (
-    <button
-      style={{
-        backgroundColor: 'salmon',
-        color: 'white',
-        padding: '10px 20px',
-        border: 0,
-        borderRadius: 10,
-        cursor: 'pointer',
-        fontSize, // === fontSize: fontSize
-      }}
-    >
-      {text}
-    </button>
-  );
-}
-// PropTypes을 활용해 각각 prop의 타입 지정 가능 → 타입 지정 실수 방지
-Btn.propTypes = {
-  text: PropTypes.string.isRequired,
-  fontSize: PropTypes.number,
-  // fontSize를 prop로 설정하지 않은 Btn이 있음 → isRequired 설정 시 오류남
-};
-
-function App() {
-  return (
-    <div>
-      <Btn text='Submit' fontSize={24}></Btn>
-      <Btn text='Reset'></Btn>
-    </div>
-  );
-}
-
+import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
